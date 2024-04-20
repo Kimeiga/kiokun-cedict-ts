7d471bfbff487896f52145a3a6544cd0988fdd55
kimeiga <hak7alp@gmail.com>
2024-04-20 12:12:37 -0400

    hi

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	let word: string = '';
+
+	function goToWord() {
+		word && goto(`/${word}`);
+	}
+</script>
+
+<h1>Kiokun</h1>
+
+<p>Chinese and Japanese Etymology Dictionary (WIP)</p>
+
+<p>Search word</p>
+
+<input type="text" bind:value={word} on:keydown={(e) => e.key === 'Enter' && goToWord()} />
+<button on:click={goToWord}>Go</button>
+
+<p>Samples:</p>
+<ul>
+	<li><a href="/2019冠狀病毒病">2019冠狀病毒病</a></li>
+</ul>
+
+<hr />
+<p>By <a href="https://hakanalpay.com">kimeiga</a></p>
